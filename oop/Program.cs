c457bf915eba6b166d2a5446d1404768a2b23519
phhung07 <phhg2907@gmail.com>
2026-08-20 16:31:02 +0700

    add day2

--- oop/Program.cs
+++ oop/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Schema;
 
 namespace oop
 {
@@ -10,6 +11,87 @@ namespace oop
     {
         static void Main(string[] args)
         {
+            Student[] liststudent =
+            {
+                new Student("Phan Tan Phat", 9),
+                new Student("Nguyen Minh Duy", 5),
+                new Student("Phan Gia Hau", 10),
+                new Student("Pham Nguyen Dang Khoa", 3),
+                new Student("Pham Hung", 8),
+            };
+            Console.WriteLine(Student.GettotalStudent());
+            foreach (Student student in liststudent)
+            {
+                Console.WriteLine
+                (
+                    $"{student.Getname()} - " +
+                    $"{student.Getscore()} - " +
+                    $"{student.GetClassification()} - " +
+                    $"{(student.IsPassed() ? "Passed" : "Failed")}"
+                );
+            }
+            Console.WriteLine(
+                $"Sinh vien diem cao nhat: {Student.FindTopStudent(liststudent).Getname()} - {Student.FindTopStudent(liststudent).Getscore()}"
+            );
+
+            Console.WriteLine(
+                $"Diem trung binh: {Student.CalculateAverageScore(liststudent)}"
+            );
+        }
+        public class Student
+        {
+            public string name;
+            public double score;
+            public static int totalStudent = 0;
+
+            public Student(string name, double score)
+            {
+                this.name = name;
+                this.score = score;
+                totalStudent++;
+            }
+            public string Getname()
+            {
+                return name;
+            }
+            public double Getscore()
+            {
+                return score;
+            }
+            public bool IsPassed()
+            {
+                return score >= 5;
+            }
+            public string GetClassification()
+            {
+                if (score >= 8) return "Excellent";
+                else if (score >= 6.5) return "Good";
+                else if (score >= 5) return "Average";
+                else return "Weak";
+            }
+            public static int GettotalStudent()
+            {
+                return totalStudent;
+            }
+            public static Student FindTopStudent(Student[] sst)
+            {
+                Student stu = sst[0];
+                foreach (Student s in sst)
+                {
+                    if (stu.score < s.score)
+                        stu = s;
+                }
+                return stu;
+            }
+            public static double CalculateAverageScore(Student[] students)
+            {
+                double tong = 0;
+                foreach (Student s in students)
+                {
+                    tong += s.score;
+                }
+                return tong / students.Length;
+            }
         }
     }
 }
